25350b7a6e2d7350b46f7b02aa258e1a1137888f
Aafreen2203 <aafreen22k@gmail.com>
2025-02-15 01:23:37 +0530

    made responsive

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const [mobile, setMobile] = useState(window.innerWidth <= 768);
+  const [menuOpened, setMenuOpened] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => setMobile(window.innerWidth <= 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <nav className="nav">
       {/* Logo Section */}
       <div className="nav-logo">NutriAI</div>
-      
+
+      {/* Mobile Menu Toggle */}
+      {mobile ? (
+        <div className="nav-toggle" onClick={() => setMenuOpened(!menuOpened)}>
+          <div className="burger"></div>
+          <div className="burger"></div>
+          <div className="burger"></div>
+        </div>
+      ) : null}
+
       {/* Menu Links */}
-      <ul className="nav-menu">
+      <ul
+        className={`nav-menu ${
+          mobile && menuOpened ? "nav-menu-open" : ""
+        }`}
+      >
         <li>
-          <Link to="/" className="nav-link">Home</Link>
+          <Link
+            to="/"
+            className="nav-link"
+            onClick={() => setMenuOpened(false)}
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about" className="nav-link">About Us</Link>
+          <Link
+            to="/about"
+            className="nav-link"
+            onClick={() => setMenuOpened(false)}
+          >
+            About Us
+          </Link>
         </li>
         <li>
-          <Link to="/login" className="nav-link">Login</Link>
+          <Link
+            to="/login"
+            className="nav-link"
+            onClick={() => setMenuOpened(false)}
+          >
+            Login
+          </Link>
         </li>
       </ul>
-
-      {/* Contact Button */}
-      {/* <div className='contact-nav-container'>
-        <Link to="/contact" className="nav-contact">Contact Us</Link>
-      </div> */}
     </nav>
   );
 };
